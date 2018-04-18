6064c2d80c7e1bbb7e595501ad6f4e52b73176cd
hubLuo <hubluo@126.com>
2018-04-19 02:35:34 +0800

    FluxBoiling： 引入dispatcher第3个中间件的功能，用中间件来处理筛选等工作，明白中间件的工作原理

--- src/boiling/fluxBoiling/calculator.js
+++ src/boiling/fluxBoiling/calculator.js
@@ -5,12 +5,24 @@ import React, { Component } from 'react';
 import TemperatureInput from './temperatrueInput';
 import BoilingVerdict from './boilingVerdict';
 import Store from "../../flux/store";
-
+import Dispatcher from "../../flux/dispatcher";
 var store =new Store();
 class Calculator extends Component {
     constructor(props) {
         super(props);
         this.state = {temperature: '', scale: 'c'};
+        Dispatcher.use(function log(actions,next){
+            setTimeout(function(){
+                console.log("中间件日志1--",actions.actionType);
+                next();
+            },2000);
+        });
+        Dispatcher.use(function log2(actions,next){
+            setTimeout(function(){
+                console.log("中间件日志2--",actions.actionType);
+                next();
+            },2000);
+        });
     }
 
     shouldComponentUpdate(){
--- src/flux/dispatcher.js
+++ src/flux/dispatcher.js
@@ -1,5 +1,5 @@
 const storeCallBackList=[];
-
+const middleWareList=[];
 /*
  dispatcher
  1、让store与action解耦。
@@ -7,13 +7,37 @@ const storeCallBackList=[];
  3、作为中间件来处理筛选等工作
  * */
 const Dispatcher={
+    //注册store执行函数
     register(storeCallBack){
         storeCallBackList.push(storeCallBack);
     },
+    //注册中间件执行函数
+    use(middleWare){
+        middleWareList.push(middleWare);
+    },
+    //作为中间件筛选过度方法，封装原先dispatch---这也是中间件的工作原理
     dispatch(actions){
+        let that=this,num=-1;
+        function next(){
+            let fn=middleWareList[++num];
+            //fn?fn(actions,next):that._dispatch(actions);
+            if(fn){
+                fn(actions,next);
+                //这里之所以把next作为fn参数返回,而不是直接递归执行完全，主要出于2点考虑
+                //1：fn的执行很可能是异步的    2：将next的控制权交回给用户。
+                //这样处理也有坏处，那就是在中间件函数中必须要写next,否则就没法执行最后的_dispatch
+                //next();
+            }else{
+                that._dispatch(actions);
+            }
+        }
+        next();
+    },
+    _dispatch(actions){
         for(let storeCallBack of storeCallBackList){
             storeCallBack(actions);
         }
     }
+
 };
 export default Dispatcher;
